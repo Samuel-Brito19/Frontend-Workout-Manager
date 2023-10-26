908f06d833d0ef1280c2e8e3efcef32ed1b59e75
Samuel Brito <sambriton7@gmail.com>
2023-10-26 16:15:34 -0300

    Finishing our Login page style

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom'
 import * as Styled from './styles'
 
 const Login = () => {
     return (
         <Styled.Container>
-            <h2>LOGIN</h2>
+            <h2>Login</h2>
             <Styled.Form>
                 <Styled.Input type='email' placeholder='Enter email'/>
                 <Styled.Input type='email' placeholder='Enter email'/>
                 <Styled.Button>ENTER</Styled.Button>
             </Styled.Form>
+            <Styled.Span>Don't you have an account? <Link to="/register">Register</Link></Styled.Span>
         </Styled.Container>
     )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
 display: flex;
 align-items: center;
+flex-direction: column;
 justify-content: center;
 //background-color: #f2f2f2;
 height: 100vh;
@@ -16,12 +17,28 @@ height: 100vh;
 `
 
 export const Form = styled.form`
-
+display: flex;
+flex-direction: column;
+padding: 50px;
+background-color:#f2f2f2;
+width: 200px;
+gap: 20px;
 `
 
 export const Input = styled.input`
-
+padding: 10px;
+border-bottom: 1px solid gray;
 `
 
 export const Button = styled.button`
+padding: 10px;
+border: none;
+background-color: teal;
+color:#242424;
+cursor: pointer;
+`
+
+export const Span = styled.span`
+font-size: 15px;
+text-align: center;
 `
