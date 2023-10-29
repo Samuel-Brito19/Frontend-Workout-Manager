9a9e6bbfc04dfda56cee49d014cd4d2d2ad0b567
Samuel Brito <sambriton7@gmail.com>
2023-10-28 21:41:08 -0300

    Creating a form component

--- src/pages/Exersices/styles.ts
+++ src/pages/Exersices/styles.ts
@@ -13,17 +13,7 @@ justify-content: center;
 background-color: #f2f2f2;
 `
 
-export const Title = styled.h3`
+export const Title = styled.h2`
 
 `
 
-export const FormContainer = styled.form`
-display: flex;
-align-items: flex-end;
-gap: 10px;
-flex-wrap: wrap;
-background-color: #fff;
-padding: 20px;
-box-shadow: 0px 0px 5px #ccc;
-border-radius: 5px;
-`
