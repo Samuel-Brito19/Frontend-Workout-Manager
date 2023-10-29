76ce62c69d9050e8f7ab07ccec9cfc7692558f3b
Samuel Brito <sambriton7@gmail.com>
2023-10-29 18:38:48 -0300

    Creating a Grid component

--- src/pages/Exersices/Exersices.tsx
+++ src/pages/Exersices/Exersices.tsx
@@ -2,13 +2,16 @@ import { toast, ToastContainer } from "react-toastify"
 import 'react-toastify/ReactToastify.css'
 import * as Styled from './styles'
 import GlobalStyle from "../../styles/global"
+import Form from "./Form/Form"
+
 
 const Exersices = () => {
     return (
 
         <>
             <Styled.Container>
-
+                <Styled.Title>Exersices</Styled.Title>
+                <Form/>
             </Styled.Container>
 
             <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
---
+++ src/pages/Exersices/Form/Form.tsx
@@ -0,0 +1,28 @@
+//import { useRef } from 'react'
+import * as Styled from './styles'
+
+const Form = () => {
+
+    //const ref = useRef()
+    return(
+
+        <Styled.FormContainer>
+            <Styled.InputArea>
+                <Styled.Label>Name</Styled.Label>
+                <Styled.Input name='name' />
+            </Styled.InputArea>
+            <Styled.InputArea>
+                <Styled.Label>Series</Styled.Label>
+                <Styled.Input name='series' />
+            </Styled.InputArea>
+            <Styled.InputArea>
+                <Styled.Label>Repetitons</Styled.Label>
+                <Styled.Input name='repetitions' />
+            </Styled.InputArea>
+
+            <Styled.Button type='submit'>SAVE</Styled.Button>
+        </Styled.FormContainer>
+    )
+}
+
+export default Form
