c18755d6e3954d445aac96a54efb93ea1ef4726f
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-11-01 09:17:25 -0300

    fix: rename exercises

---
+++ src/pages/Exercises/Exercises.tsx
@@ -0,0 +1,23 @@
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css";
+import * as Styled from "./styles";
+import GlobalStyle from "../../styles/global";
+import Form from "./Form/Form";
+import Grid from "./Grid/Grid";
+
+const Exercises = () => {
+  return (
+    <>
+      <Styled.Container>
+        <Styled.Title>Exersices</Styled.Title>
+        <Form />
+        <Grid />
+      </Styled.Container>
+
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default Exercises;
