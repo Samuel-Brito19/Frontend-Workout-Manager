8de9c3a34f4f0d67a9ecc9dd00536f17ab69c24d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-11-01 09:50:17 -0300

    feat: add state to login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,12 @@
 import { Link, useNavigate } from "react-router-dom";
 import * as Styled from "./styles";
+import { useState } from "react";
 
 const Login = () => {
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -13,8 +17,18 @@ const Login = () => {
     <Styled.Container>
       <h2>Login</h2>
       <Styled.Form onSubmit={handleSubmit}>
-        <Styled.Input type="email" placeholder="Enter email" />
-        <Styled.Input type="password" placeholder="Enter password" />
+        <Styled.Input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Styled.Input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
         <Styled.Button type="submit">ENTER</Styled.Button>
         <Styled.Span>
           Don't have an account yet? <Link to="/register">Register</Link>
