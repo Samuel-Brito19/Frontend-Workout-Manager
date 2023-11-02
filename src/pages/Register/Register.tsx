74578a8bf69a78bc30f10d81150609c1b75a74cd
Samuel Brito <sambriton7@gmail.com>
2023-11-02 19:53:57 -0300

    Creating the logic in our Register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,38 +1,59 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import * as Styled from './styles'
 import { useState } from 'react'
+import api from '../../services/api'
+import { AxiosError } from 'axios'
 
 const Register = () => {
 
     const [inputs, setInputs] = useState({
-        username: '',
+        name: '',
         email: '',
-        password: ''
+        password: '',
+        passwordConf: ''
     })
+    
+    //const [passwordConf, setPasswordConf] = useState("")
+
+
+    const navigate = useNavigate()
 
     const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
     }
 
+
     const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
         e.preventDefault()
-        // try {
-        //     const res = await axios.post('/register', inputs)
-        //     console.log(res)
+        try {
+            await api.post('/users', inputs)
+            console.log(inputs)
+            
+            if(inputs.password !== inputs.passwordConf) {
+                alert('password does not match!!')
+                return
+            }
+            
+            navigate("/")
+
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                alert(error.response?.data.error);
+                return;}
+            }
+        }
     
-        // } catch (error) {
-        //     console.log(error)
-        // }
-    }
 
     console.log(inputs)
     return (
         <Styled.Container>
         <h2>Register</h2>
         <Styled.Form>
-            <Styled.Input required type='text' placeholder='Username' name='username' onChange={handleChange}/>
+            <Styled.Input required type='text' placeholder='Username' name='name' onChange={handleChange}/>
             <Styled.Input required type='email' placeholder='Enter email' name='email' onChange={handleChange}/>
             <Styled.Input required type='password' placeholder='Enter password' name='password' onChange={handleChange}/>
+            <Styled.Input required type='password' placeholder='Confirm password' name='passwordConf' onChange={handleChange}/>
+            <Styled.LabelError></Styled.LabelError>
             <Styled.Button onClick={handleSubmit}>REGISTER</Styled.Button>
             <Styled.Span>Already have an account? <Link to="/">Login</Link></Styled.Span>
         </Styled.Form>
